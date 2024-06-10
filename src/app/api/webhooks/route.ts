68633829fd3582a04436757b0d2e6bd64760c7e5
martinromario55 <martinromario55@gmail.com>
2024-06-10 20:17:28 +0300

    changing sender email address

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -70,7 +70,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "CaseCobra <products@casecobra.com>",
+        from: "CaseCobra <onboarding@resend.dev>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
